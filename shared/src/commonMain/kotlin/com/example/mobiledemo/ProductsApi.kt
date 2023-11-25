f798a0adb83e085d721fd1ade5a89de4949078a9
billyyan <By869422>
2023-11-25 14:13:27 -0800

    Implement ProductsApi

---
+++ shared/src/commonMain/kotlin/com/example/mobiledemo/ProductsApi.kt
@@ -0,0 +1,43 @@
+package com.example.mobiledemo
+
+import co.touchlab.kermit.Logger
+import com.example.mobiledemo.Constants.BASE_URL
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.serialization.json.Json
+
+class ProductsApi {
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    fun fetchProducts(limit: Int): Flow<RequestState> {
+        return flow {
+            emit(RequestState.Loading)
+            delay(2000)
+            try {
+                emit(
+                    RequestState.Success(
+                        data = httpClient.get(urlString = "${BASE_URL}products?limit=$limit").body()
+                    )
+                )
+            } catch (e: Exception) {
+                Logger.setTag("ProductsApi")
+                Logger.e { e.message.toString() }
+                emit(RequestState.Error(message = "Error while fetching the data."))
+            }
+        }
+    }
+}
